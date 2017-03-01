222ec7b2049eb3978994e398f5660e2187d54921
Mauro Foti <m.foti@teckhouse.com>
2017-03-01 12:29:11 +0100

    Adding headers application/json

--- lib/NajiDev/XbmcApi/Transport/HttpTransport.php
+++ lib/NajiDev/XbmcApi/Transport/HttpTransport.php
@@ -9,58 +9,70 @@ use \NajiDev\XbmcApi\Exception\UnauthorizedException;
 
 class HttpTransport implements Transport
 {
-	protected $user;
-	protected $password;
+    public $timeout = 15;
+    protected $user;
+    protected $password;
 
-	public $timeout = 15;
+    private $dsn;
 
-	public function __construct($host, $port = 80, $user = null, $password = null)
-	{
-		$this->dsn = $host . ':' . $port . '/jsonrpc';
+    public function __construct($host, $port = 80, $user = null, $password = null)
+    {
+        $this->dsn = $host . ':' . $port . '/jsonrpc';
 
-		$this->user     = $user;
-		$this->password = $password;
-	}
+        $this->user     = $user;
+        $this->password = $password;
+    }
 
-	public function request($data)
-	{
-		$handle = curl_init();
+    public function request($data)
+    {
+        $handle = curl_init();
 
-		curl_setopt($handle, CURLOPT_URL, 'http://' . $this->dsn);
-		curl_setopt($handle,CURLOPT_USERPWD, $this->user . ':' . $this->password);
-		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, $this->timeout);
-		curl_setopt($handle, CURLOPT_TIMEOUT, $this->timeout);
+        curl_setopt($handle, CURLOPT_URL, 'http://' . $this->dsn);
+        curl_setopt($handle, CURLOPT_USERPWD, $this->user . ':' . $this->password);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, $this->timeout);
+        curl_setopt($handle, CURLOPT_TIMEOUT, $this->timeout);
 
-		curl_setopt($handle, CURLOPT_POST, true);
-		curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($handle, CURLOPT_POST, true);
+        curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($handle, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data))
+        );
 
-		$response = curl_exec($handle);
-		$httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+        $response = curl_exec($handle);
+        $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
-		if (403 === $httpCode)
-			throw new UnauthorizedException();
 
-		$response = json_decode($response);
-		if (isset($response->error))
-		{
-			$stackException = null;
-			if (isset($response->error->data))
-			{
-				$stackException = new InvalidArgumentException(
-					'In ' . $response->error->data->method . ': ' . $response->error->data->stack->message
-				);
-			}
+        error_log('http://' . $this->dsn);
+        error_log($data);
+        error_log($httpCode);
+        error_log($response);
 
-			throw new InvalidArgumentException(
-				$response->error->message,
-				$response->error->code,
-				$stackException
-			);
-		}
 
-		return $response->result;
-	}
+        if (403 === $httpCode) {
+            throw new UnauthorizedException();
+        }
+
+        $response = json_decode($response);
+
+        if (isset($response->error)) {
+            $stackException = null;
+            if (isset($response->error->data)) {
+                $stackException = new InvalidArgumentException(
+                    'In ' . $response->error->data->method . ': ' . $response->error->data->stack->message
+                );
+            }
+
+            throw new InvalidArgumentException(
+                $response->error->message,
+                $response->error->code,
+                $stackException
+            );
+        }
+
+        return $response->result;
+    }
 }
